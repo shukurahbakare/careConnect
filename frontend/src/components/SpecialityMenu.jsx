81ece8ea4941d3e6d6a580b8cd723a1ecd7f331d
shukurah <shuksbakare@gmail.com>
2025-01-23 23:29:33 +0100

    completed header styling

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SpecialityMenu = () => {
+  return (
+    <div id='speciality'>  
+        <h1>Find by speciality</h1>
+        <p>Browse through our extensive list of trusted doctors,<br className='hidden sm:block'/> schedule your appointment hassle-free.</p>
+    </div>
+  )
+}
+
+export default SpecialityMenu
